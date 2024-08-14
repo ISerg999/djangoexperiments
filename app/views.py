ed224b8f97524fa3c329047339595a05e96cc4bc
Serg I. <iserg009@yandex.ru>
2024-08-14 19:18:29 +0300

    Шаблоны html

--- app/views.py
+++ app/views.py
@@ -21,5 +21,15 @@ def user(request, name: str = "Undefined", age: int = 16):
     return render(request, "user.html", context=data)
 
 
+def postuser(request):
+    name = request.POST.get("name", "Безимянный")
+    age = request.POST.get("age", 16)
+    langs = request.POST.getlist("languages", ["python",]) # Также с select формы
+    return HttpResponse(f"""
+        <p>Имя: {name}  Возраст: {age}</p>
+        <p>Языки: {langs}</p>
+    """)
+
+
 def json(request, name: str = "Undefined", age: int = 16):
     return JsonResponse({"name": name, "age": age})
